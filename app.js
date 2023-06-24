cc86db3da9b95a6914cfe828188a8e40312a3a33
shinjiiiiiii <udauvergne@edenSchool.fr>
2023-06-24 14:07:43 +0200

    add all without vite

---
+++ app.js
@@ -0,0 +1,40 @@
+//grafikart
+
+const spies = document.querySelectorAll('[data-spy]')
+let observer = null
+
+const activate = (elem) => {
+  const id = elem.getAttribute('id')
+  const anchor = document.querySelector(`nav  a[href="#${id}"]`)
+  console.log(anchor);
+  if (anchor === null) {
+    return null
+  }
+  const anchorsActive = document.querySelectorAll(`nav a.active`)
+    .forEach(node => node.classList.remove('active'))
+  anchor.classList.add('active')
+}
+const callback = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            activate(entry.target)
+        }
+    })
+  }
+const observe = (elems) => {
+    if(observer !== null) {
+        elems.forEach(elem => observer.unobserve(elem))
+    }
+    observer = new IntersectionObserver(callback, {
+        rootMargin: `-50% 0px`
+    })
+    spies.forEach((spy) => {
+        observer.observe(spy)
+    })
+} 
+  if(spies.length > 0) {
+    observe(spies)
+    window.addEventListener('resize',() => {
+        observe(spies)
+    })
+  }
